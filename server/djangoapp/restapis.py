11207309dccd37ba1fd58f889f8b269923f1d1a1
janimatyas72527 <jani.matyas@gmail.com>
2023-06-09 09:46:12 -0400

    Fixes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,6 @@
 import json
 import http.client
-from datetime import date
+from datetime import datetime
 from .models import CarDealer, DealerReview
 
 # Create a `get_request` to make HTTP GET requests
@@ -16,7 +16,7 @@ def get_request(package_name, action_name, params):
 
         headers = {
             'accept': "application/json",
-            'authorization': "<Auth Token>",
+            'authorization': "Bearer eyJraWQiOiIyMDIzMDUxMTA4MzEiLCJhbGciOiJSUzI1NiJ9.eyJpYW1faWQiOiJJQk1pZC0zMTAwMDBBUDVYIiwiaWQiOiJJQk1pZC0zMTAwMDBBUDVYIiwicmVhbG1pZCI6IklCTWlkIiwianRpIjoiNTlmMjI4MjYtNTE1OS00MjBlLWFiNjEtYmE1ZDU5YWIzNTMyIiwiaWRlbnRpZmllciI6IjMxMDAwMEFQNVgiLCJnaXZlbl9uYW1lIjoiTWF0eWFzIiwiZmFtaWx5X25hbWUiOiJKYW5pIiwibmFtZSI6Ik1hdHlhcyBKYW5pIiwiZW1haWwiOiJqYW5pLm1hdHlhc0BnbWFpbC5jb20iLCJzdWIiOiJqYW5pLm1hdHlhc0BnbWFpbC5jb20iLCJhdXRobiI6eyJzdWIiOiJqYW5pLm1hdHlhc0BnbWFpbC5jb20iLCJpYW1faWQiOiJJQk1pZC0zMTAwMDBBUDVYIiwibmFtZSI6Ik1hdHlhcyBKYW5pIiwiZ2l2ZW5fbmFtZSI6Ik1hdHlhcyIsImZhbWlseV9uYW1lIjoiSmFuaSIsImVtYWlsIjoiamFuaS5tYXR5YXNAZ21haWwuY29tIn0sImFjY291bnQiOnsidmFsaWQiOnRydWUsImJzcyI6IjVhMzk1ODJkNWE4NjRjYzM5YjYzYzMwY2VlMDU2YmZjIiwiaW1zX3VzZXJfaWQiOiIxMDgxMDQzMyIsImltcyI6IjI2MzI3NTUifSwiaWF0IjoxNjg2MzE2Nzg4LCJleHAiOjE2ODYzMjAzODgsImlzcyI6Imh0dHBzOi8vaWFtLmNsb3VkLmlibS5jb20vaWRlbnRpdHkiLCJncmFudF90eXBlIjoidXJuOmlibTpwYXJhbXM6b2F1dGg6Z3JhbnQtdHlwZTpwYXNzY29kZSIsInNjb3BlIjoiaWJtIG9wZW5pZCIsImNsaWVudF9pZCI6ImJ4IiwiYWNyIjoxLCJhbXIiOlsicHdkIl19.cU4x6eGVlHpkrJam5mrKFksGSWZ5kzEhVyJkB2U-Rkcmp0_GqehmhVPGVrgwoHHX1_shDfrJwS2pmsUcaq3aD8ZUUI_2AOhVnFNal-fjV6gYvRY4Ui7F4lNgko-nE6AcUvRX1jzWnkApokg177lAOxHX4Ws2vksj-Lq3Lx3ZPjpgfm8WrLZGwbDNtkfyoCHEV80lNfdQ7Pz0kXR4oJxYcy9Z1oxyM__yPzXYNQv9rvu3qxureJqu66MIogh9qa8D2X25n2nGWWHuqDXSLsPNNmikPtBolnXEmXeyQ7P_mzpkVHFXn-ABvXXk5kILLR2xTATSzmTmizjFuNS62Vxpjw",
             'content-type': "application/json"
             }
 
@@ -55,25 +55,43 @@ def get_dealers_from_cf(params):
 
 
 def get_dealer_review(json_data):
-    return DealerReview(
-        int(json_data['id']),
-        json_data['name'],
-        json_data['dealership'],
-        json_data['review'],
-        bool(json_data['purchase']),
-        date.fromisoformat(json_data['purchase_date']),
-        json_data['car_make'],
-        json_data['car_model'],
-        int(json_data['car_year'])
-        )
+    if 'id' in json_data:
+        review_id = int(json_data['id'])
+    else:
+        review_id = -1
+    purchase = bool(json_data['purchase'])
+    if (purchase):
+        return DealerReview(
+            review_id,
+            json_data['name'],
+            json_data['dealership'],
+            json_data['review'],
+            purchase,
+            datetime.strptime(json_data['purchase_date'], '%m/%d/%Y'),
+            json_data['car_make'],
+            json_data['car_model'],
+            int(json_data['car_year'])
+            )
+    else:
+        return DealerReview(
+            review_id,
+            json_data['name'],
+            json_data['dealership'],
+            json_data['review'],
+            purchase,
+            None,
+            None,
+            None,
+            None
+            )
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 def get_dealer_review_by_id_from_cf(dealer_id):
-    result = get_request('review-package', 'get-review', {'id': dealer_id})
-    return map(result.reviews)
+    result = get_request('review-package', 'get-review', {})
+    return map(get_dealer_review, result['reviews'])
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
